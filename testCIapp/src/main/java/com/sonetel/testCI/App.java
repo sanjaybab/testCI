ac58cbc652fb7b81a0050e6bfaf85eaf12bea924
sanjaybab <sanjay.baberwal@sonetel.com>
2017-10-07 14:09:51 +0530

    hello world

---
+++ testCIapp/src/main/java/com/sonetel/testCI/App.java
@@ -0,0 +1,13 @@
+package com.sonetel.testCI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!.. see this" );
+    }
+}
