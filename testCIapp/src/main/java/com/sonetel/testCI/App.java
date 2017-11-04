c0e2c5f6f52493000a30ae9be4d6030a60874bea
sanjaybab <31980835+sanjaybab@users.noreply.github.com>
2017-11-04 12:33:51 +0530

    Update App.java
    world.

--- testCIapp/src/main/java/com/sonetel/testCI/App.java
+++ testCIapp/src/main/java/com/sonetel/testCI/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!.. see this, new addition" );
-        System.out.println( "22222222222 see that, new addition" );
+        System.out.println( "Hello Woralldd see that, new addition" );
         System.out.println( "22222222222 see that, new addition" );
         System.out.println( "333333333333333 see that, new addition" );
         System.out.println( "22222222222 see that, new addition" );
