e5fb5d228b1131595a1b613fff73637ea79770e2
sanjaybab <31980835+sanjaybab@users.noreply.github.com>
2017-11-07 11:22:02 +0530

    Update App.java
    additional

--- testCIapp/src/main/java/com/sonetel/testCI/App.java
+++ testCIapp/src/main/java/com/sonetel/testCI/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println( "333333333333333 see that, new addition" );
         System.out.println( "22222222222 see that, new addition" );
         System.out.println( "something meaningful to see !!!!!, new addition" );
+        System.out.println( "something meaningful to see !!!!!, new addition addition...." );
     }
 }
